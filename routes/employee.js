6f8f58da05879a5191a3b5c548b5c3dc1fec8d76
saurabh451 <saurabh@veefin.com>
2023-09-26 16:24:23 +0530

    deploye

--- routes/employee.js
+++ routes/employee.js
@@ -2,10 +2,10 @@ const express=require('express');
 const router=express.Router();
 const Employee = require('../controller/employee');
 
-router.post('/employee',[],Employee.createEmp);
-router.put('/employee/:id',[],Employee.updateEmp)
-router.get('/employee/:id',[],Employee.getEmpById);
-router.delete('/employee/:id',[],Employee.deleteEmp);
-router.get('/employee',[],Employee.getAllEmployee);
+router.post('/employee',[],Employee.createEmp);  // for adding employee
+router.put('/employee/:id',[],Employee.updateEmp) // for update employee
+router.get('/employee/:id',[],Employee.getEmpById); // for getting employee by there id
+router.delete('/employee/:id',[],Employee.deleteEmp); // for delete employee record
+router.get('/employee',[],Employee.getAllEmployee); // for get all employee record
 
 module.exports=router;
